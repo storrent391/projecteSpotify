34343e6c605ec96203ffd11adfe42170991327db
storrent391 <storrent391@boscdelacoma.cat>
2025-01-21 17:03:20 +0100

    IMPLEMENTING Form

--- spotifyAngular/src/app/components/register/register.component.ts
+++ spotifyAngular/src/app/components/register/register.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
 }
