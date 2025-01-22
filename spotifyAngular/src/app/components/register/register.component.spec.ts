11599fd17df6bb2f08018300013501c9457d0e01
storrent391 <storrent391@boscdelacoma.cat>
2025-01-22 16:33:28 +0100

    IMPLEMENTED register component

--- spotifyAngular/src/app/components/register/register.component.spec.ts
+++ spotifyAngular/src/app/components/register/register.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegisterComponent } from './register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 
 describe('RegisterComponent', () => {
@@ -19,22 +20,5 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should have a disabled submit button if the form is invalid', () => {
-    const submitButton = fixture.debugElement.query(By.css('button[type="submit"]')).nativeElement;
-    expect(submitButton.disabled).toBeTrue();
-  });
-  it('should enable the submit button when the form is valid', () => {
-    component.registerForm.setValue({
-      username: 'testuser',
-      email: 'test@example.com',
-      password: 'Password123',
-      confirmPassword: 'Password123',
-    });
-    fixture.detectChanges();
-  
-    const submitButton = fixture.debugElement.query(By.css('button[type="submit"]')).nativeElement;
-    expect(submitButton.disabled).toBeFalse();
   });
 });
--- spotifyAngular/src/app/components/register/register.component.ts
+++ spotifyAngular/src/app/components/register/register.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
   registerForm: FormGroup;
@@ -12,8 +16,16 @@ export class RegisterComponent {
     this.registerForm = this.fb.group({
       username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', Validators.required],
     });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log('Form Submitted', this.registerForm.value);
+    } else {
+      console.error('Form is invalid');
+    }
   }
 }
