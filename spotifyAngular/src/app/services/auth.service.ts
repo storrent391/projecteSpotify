74a586cd00c770424780f4dbcfae9bf036fe5afa
storrent391 <storrent391@boscdelacoma.cat>
2025-01-23 19:07:07 +0100

    REFACTOR auth service

--- spotifyAngular/src/app/services/auth.service.ts
+++ spotifyAngular/src/app/services/auth.service.ts
@@ -6,11 +6,15 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/api/auth';
+  private baseUrl = 'http://localhost:5000/auth'; // URL base del backend
 
   constructor(private http: HttpClient) {}
 
+  register(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.baseUrl}/register`, { email, password });
+  }
+
   login(email: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, { email, password });
+    return this.http.post(`${this.baseUrl}/login`, { email, password });
   }
 }
