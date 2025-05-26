ec3b1096af418a81ad14454a8f325fc5d8deb3e8
ALZAMORA1\storrent <storrent@alzamoragroup.com>
2025-05-26 09:56:06 +0200

    TEST should allow activation when authenticated and should block activation and redirect to login when not authenticated

--- spotifyAngular/src/app/core/guards/auth.guard.spec.ts
+++ spotifyAngular/src/app/core/guards/auth.guard.spec.ts
@@ -1,17 +1,51 @@
+// src/app/guards/auth.guard.spec.ts
+
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
-import { authGuard } from './auth.guard';
 
-describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+describe('AuthGuard (TDD)', () => {
+  let guard: AuthGuard;
+  let authSpy: jasmine.SpyObj<AuthService>;
+  let router: Router;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    authSpy = jasmine.createSpyObj('AuthService', ['isAuthenticated']);
+
+    TestBed.configureTestingModule({
+      
+      providers: [
+        provideRouter([]),
+        { provide: AuthService, useValue: authSpy }
+      ]
+    });
+
+    guard = TestBed.inject(AuthGuard);
+    router = TestBed.inject(Router);
+    spyOn(router, 'navigate');
   });
 
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+  it('should allow activation when authenticated', () => {
+    authSpy.isAuthenticated.and.returnValue(true);
+
+    const result = guard.canActivate({} as any, { url: '/protected' } as any);
+
+    expect(result).toBeTrue();
+    expect(router.navigate).not.toHaveBeenCalled();
+  });
+
+  it('should block activation and redirect to /login when not authenticated', () => {
+    authSpy.isAuthenticated.and.returnValue(false);
+
+    const result = guard.canActivate({} as any, { url: '/protected' } as any);
+
+    expect(result).toBeFalse();
+    expect(router.navigate).toHaveBeenCalledWith(
+      ['login'],
+      { queryParams: { returnUrl: '/protected' } }
+    );
   });
 });
